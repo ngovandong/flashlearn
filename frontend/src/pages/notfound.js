6eee5f5f35137999fc3f29edd15447b88356afa8
ngovandong <dongngo2001@gmail.com>
2023-04-02 20:59:32 +0700

    feat: not found page

--- frontend/src/pages/notfound.js
+++ frontend/src/pages/notfound.js
@@ -1,10 +1,18 @@
 import React from "react";
-function NotFound() {
+
+const NotFound = () => {
   return (
-    <div>
-      <p>There's nothing here!</p>
+    <div className="error-container">
+      <div className="error-text">
+        <h1>404</h1>
+        <h2>Oops! Page not found</h2>
+        <p>We're sorry, the page you're looking for cannot be found.</p>
+        <a href="/">Return to home</a>
+      </div>
+      <div className="error-image">
+      </div>
     </div>
   );
-}
+};
 
 export default NotFound;
