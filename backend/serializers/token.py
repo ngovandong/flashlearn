f09f0336742a7ab9eb4643cdbaec1dacc75edb23
Ngo Van Dong <dongngo2001@gmail.com>
2023-04-17 17:21:34 +0700

    fix: active acount by email

--- backend/serializers/token.py
+++ backend/serializers/token.py
@@ -1,7 +1,10 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, TokenRefreshSerializer
 from rest_framework_simplejwt.settings import api_settings
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from django.contrib.auth.models import update_last_login
 from .user import UserSerializer
+from ..models import User
 
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -17,36 +20,37 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
         if not self.user.is_validated_email:
-            raise serializers.ValidationError({"errors": "Please activate your email account!"})
+            raise serializers.ValidationError(
+                {"errors": "Please activate your email account!"})
         return data
 
 
 class ActiveAccountSerializer(TokenRefreshSerializer):
+    user_id = None
+
+    @classmethod
+    def get_token(cls, user):
+        token = cls.token_class.for_user(user)
+        # Add custom claims
+        serializer = UserSerializer(instance=user)
+        token['user'] = serializer.data
+        # ...
+        return token
+
     def validate(self, attrs):
-        refresh = self.token_class(attrs["refresh"])
-        # create new property
-        self._user_id = refresh.payload['user_id']
+        data = super().validate(attrs)
+        refresh = self.token_class(data['refresh'])
+        user_id = refresh.payload['user_id']
+        self.user_id = user_id
+        user = User.objects.filter(id=user_id).first()
+        if user is None:
+            raise ValidationError("User not found")
+        refresh = self.get_token(user)
 
-        data = {"access": str(refresh.access_token)}
+        data["refresh"] = str(refresh)
+        data["access"] = str(refresh.access_token)
 
-        if api_settings.ROTATE_REFRESH_TOKENS:
-            if api_settings.BLACKLIST_AFTER_ROTATION:
-                try:
-                    # Attempt to blacklist the given refresh token
-                    refresh.blacklist()
-                except AttributeError:
-                    # If blacklist app not installed, `blacklist` method will
-                    # not be present
-                    pass
-
-            refresh.set_jti()
-            refresh.set_exp()
-            refresh.set_iat()
-
-            data["refresh"] = str(refresh)
+        if api_settings.UPDATE_LAST_LOGIN:
+            update_last_login(None, user)
 
         return data
-
-    @property
-    def user_id(self):
-        return self._user_id
