443c9c4fee43f873910a26c3cd6f051cb0b550c3
ngovandong <dongngo2001@gmail.com>
2023-05-11 22:50:13 +0700

    down speed speech reading

--- frontend/src/api-service/voiceService.js
+++ frontend/src/api-service/voiceService.js
@@ -1,5 +1,6 @@
 const { SpeechSynthesis } = window.speechSynthesis;
 export const speak = (text) => {
   let utterance = new SpeechSynthesisUtterance(text);
+  utterance.rate = 0.8;
   speechSynthesis.speak(utterance);
 };
