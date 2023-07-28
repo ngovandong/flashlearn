c08619d8cca27708b9e4759e8baca5ef8503ea64
Ngo Van Dong <dongngo2001@gmail.com>
2023-07-28 09:56:41 +0700

    set timezone vietnam

--- core/settings.py
+++ core/settings.py
@@ -131,7 +131,8 @@ LANGUAGE_CODE = 'en-us'
 
 USE_I18N = True
 
-USE_TZ = False
+USE_TZ = True
+TIME_ZONE = 'Asia/Ho_Chi_Minh'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
